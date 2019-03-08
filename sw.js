2be9022824ebd603dbd2500e36b294d4ca581cb4
hisayuki <syuaibsyuaib@gmail.com>
2019-03-09 01:18:35 +0800

    fetch dari mozilla

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-self.addEventListener('install', function(event){
-	event.waitUntil(
-	caches.open('v1')
-.then(function(cache){
-		return cache.addAll([
-		'/sw/',
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+     		'/sw/',
 		'/sw/index.html',
-		'/sw/manifest.json',
 		'/sw/style.css',
 		'/sw/favicon.ico'
-		])
-		})
-	)
-	});
-	
+      ]);
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
