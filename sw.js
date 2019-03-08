897cf94b9655f346a4c032c27403a24a52a0b00d
hisayuki <syuaibsyuaib@gmail.com>
2019-03-09 01:13:26 +0800

    copy
    copy fetch dari mozilla dev, anehnya pada promise mereka menggunakan fungsi dalam fungsi

--- sw.js
+++ sw.js
@@ -15,9 +15,12 @@ self.addEventListener('install', function(event){
 	
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
+    caches.match(event.request)
+	  .then(function(resp) {
+      return resp || fetch(event.request)
+	      .then(function(response) {
+        return caches.open('v1')
+		.then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
         });  
