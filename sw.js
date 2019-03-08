1a2d95108af13086fd1fa8e0a4eb2ad0395cbe04
hisayuki <syuaibsyuaib@gmail.com>
2019-03-08 18:51:38 +0800

    From my hp

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event){
+	event.waitUntil(
+	caches.open('v1')
+.then(function(cache){
+		return cache.addAll([
+		'/index.html',
+		'/manifest.json',
+		'/'
+		])
+		})
+	)
+	});
+	
+self.addEventListener('fetch', function(e){
+	e.respondWith(
+	 caches.match(e.request)
+	.then(function(resp){
+		return resp || fetch(e.request);
+		})
+	)
+});
